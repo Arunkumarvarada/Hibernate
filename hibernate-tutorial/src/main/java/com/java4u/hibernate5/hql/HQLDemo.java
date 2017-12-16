aec73f7ba7bf9ced4f172afadcaba4312352d013
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-12-16 13:30:34 +0530

    Implemented the HQL for the Select queries

---
+++ hibernate-tutorial/src/main/java/com/java4u/hibernate5/hql/HQLDemo.java
@@ -0,0 +1,40 @@
+package com.java4u.hibernate5.hql;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.java4u.hibernate5.model.Department;
+import com.java4u.hibernate5.util.HibernateUtilMySql;
+
+public class HQLDemo {
+
+	@SuppressWarnings({ "rawtypes", "deprecation", "unchecked" })
+	public static void main(String[] args) {
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = HibernateUtilMySql.getSessionFactory().openSession();
+			transaction = session.beginTransaction();
+			Query query = session.createQuery("from Department");
+			List<Department> departments = query.list();
+
+			transaction.commit();
+			System.out.println(departments.size());
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+				System.out.println("The Trasaction is rolled back !!");
+			}
+			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+		HibernateUtilMySql.shutdown();
+	}
+
+}
